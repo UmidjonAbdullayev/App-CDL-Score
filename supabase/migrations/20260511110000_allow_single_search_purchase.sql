0ab15902e780fbf4941773d67f03f876ade68ea9
UmidjonAbdullayev <89458750+UmidjonAbdullayev@users.noreply.github.com>
2026-06-05 11:29:35 -0700

    Delete supabase directory

--- supabase/migrations/20260511110000_allow_single_search_purchase.sql
+++
@@ -1,4 +0,0 @@
-# Allow purchase_requests to have search_count >= 1 (for first-time offer)
-
-ALTER TABLE purchase_requests DROP CONSTRAINT purchase_requests_search_count_check;
-ALTER TABLE purchase_requests ADD CONSTRAINT purchase_requests_search_count_check CHECK (search_count >= 1);
--- supabase/migrations/20260511120000_add_used_first_time_offer.sql
+++
@@ -1,3 +0,0 @@
-# Add used_first_time_offer field to companies table
-
-ALTER TABLE companies ADD COLUMN used_first_time_offer boolean DEFAULT false;
